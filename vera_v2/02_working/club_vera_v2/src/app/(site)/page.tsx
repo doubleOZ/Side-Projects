9c39bd2b1576d65f886d9abef725f92bd560a126
doubleOZ <monsterdaniel97@gmail.com>
2023-09-27 18:35:47 +0800

    reorganized folder structure

---
+++ vera_v2/02_working/club_vera_v2/src/app/(site)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="h-screen flex  justify-center">
+      <div className="text-white font-medium text-3xl font-mono">
+        Welcome to Club Vera
+      </div>
+    </div>
+  );
+}
